3c91020c9cec47a543e6c7a29251cdbebd00b411
EricSoklowski <ericsoko2002@gmail.com>
2022-11-03 10:30:20 -0400

    finish product problem and lab

--- python_functions.py
+++ python_functions.py
@@ -10,6 +10,23 @@ def largest(nums):
   nums.sort()
   return nums[-1]
 
-print(largest([3, 5, 10]))
-print(largest([15, 5, 2]))
-print(largest([5, 12, 10]))
+# print(largest([3, 5, 10]))
+# print(largest([15, 5, 2]))
+# print(largest([5, 12, 10]))
+
+def occurances(str1, str2):
+  adapt_str = str1.replace(str2, '')
+  return (len(str1) - len(adapt_str)) // len(str2)
+
+# print(occurances('RIP Coolio man omg', 'o'))
+# print(occurances('where are my timberland boots', 'er'))
+
+def product(*args):
+  product = 1
+  for arg in args:
+    product *= arg
+  return product
+
+# print(product(3, 5, 12 , 4))
+# print(product(3, 5, 12 ))
+# print(product(3, 5))
